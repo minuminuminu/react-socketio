b397c439708003282df6eb6a61c0f391d2053a31
Minu <minu.kim2003@gmail.com>
2022-02-03 15:07:20 +0100

    experimenting with socketio

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,37 @@ import io from "socket.io-client";
 const socket = io.connect("http://localhost:3001");
 
 function App() {
-  return <>Hello World</>;
+  const joinRoom = () => {
+    socket.emit("join_room", "lmao");
+  };
+
+  const leaveRoom = () => {
+    socket.emit("send");
+  };
+
+  socket.on("test", (data) => {
+    console.log(data);
+  });
+
+  return (
+    <>
+      Hello World
+      <button
+        onClick={() => {
+          joinRoom();
+        }}
+      >
+        JOIN
+      </button>
+      <button
+        onClick={() => {
+          leaveRoom();
+        }}
+      >
+        LAVE
+      </button>
+    </>
+  );
 }
 
 export default App;
